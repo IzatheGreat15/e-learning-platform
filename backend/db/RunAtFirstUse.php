ef6465830877ae9a58bc9a7d0bce747e165e55d8
Jay Tejada <20103300@usc.edu.ph>
2022-12-01 07:35:01 +0800

    db modifications

--- backend/db/RunAtFirstUse.php
+++ backend/db/RunAtFirstUse.php
@@ -1,35 +1,34 @@
 <?php
-    // include("initial_db.php");
+    include("initial_db.php");
 
     include("config.php");
     if ($db->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
 
-    // include("create_users_table.php");
-    // include("create_sections_table.php");
-    // include("create_enrollments_table.php");
-    // include("create_subjects_table.php");
-    // include("create_subject_group_table.php");
+    include("create_users_table.php");
+    include("create_sections_table.php");
+    include("create_enrollments_table.php");
+    include("create_subjects_table.php");
+    include("create_subject_group_table.php");
     
-    // include("create_modules_table.php");
-    // include("create_pages_table.php");
-
-    include("create_assignment_files_table.php");
+    include("create_modules_table.php");
+    include("create_pages_table.php");
     include("create_page_files_table.php");
 
-    // include("create_quizzes_table.php");
-    // include("create_quiz_items_table.php");
-    // include("create_quiz_responses_table.php");
+    include("create_quizzes_table.php");
+    include("create_quiz_items_table.php");
+    include("create_quiz_responses_table.php");
     
-    // include("create_assignments_table.php");
-    // include("create_assignment_responses_table.php");
+    include("create_assignments_table.php");
+    include("create_assignment_responses_table.php");
+    include("create_assignment_files_table.php");
 
-    // include("create_class_announcements_table.php");
-    // include("create_subject_announcements_table.php");
+    include("create_class_announcements_table.php");
+    include("create_subject_announcements_table.php");
 
-    // include("create_threads_table.php");
-    // include("create_messages_table.php");
+    include("create_threads_table.php");
+    include("create_messages_table.php");
 
     $db->close();
 ?>
--- backend/db/create_subject_group_table.php
+++ backend/db/create_subject_group_table.php
@@ -3,6 +3,7 @@
 // sql to create table
 $sql = "CREATE TABLE subject_group (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    subject_group_name VARCHAR(64),
     subject_id INT(6) UNSIGNED,
     section_id INT(6) UNSIGNED,
     created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
