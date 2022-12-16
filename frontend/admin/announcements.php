7e8a0bb00e810db0f050898d51080b12fa10fb32
IzatheGreat15 <20100869@usc.edu.ph>
2022-12-16 10:46:05 +0800

    announcement admin fix

--- frontend/admin/announcements.php
+++ frontend/admin/announcements.php
@@ -75,7 +75,7 @@
                         <?php foreach($db->query($announcement_query) as $announcement): ?>
                         <div class="flex-col mx-20">
                             <div class="left-align blue">
-                                <div class="p-10 text">
+                                <div class="p-10 text title">
                                     <?= $announcement["announcement_title"] ?>
                                 </div>
                                 <!-- FOR TEACHERS ONLY - DELETE BUTTON -->
@@ -83,7 +83,7 @@
                                 echo '
                                 <div class="centered-align">
                                     <div class="btn">
-                                        <img src="../images/x-white.png" class="small del" alt="delete" style="width: 20px;" id="'.$announcement['id'].'">
+                                        <img src="../images/x-white.png" class="small del" alt="delete" style="width: 16px; height: 16px;" id="'.$announcement['id'].'">
                                     </div>
                                 </div>
                                 ' ?>
