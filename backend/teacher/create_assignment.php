11467cd28c43a047d871b9c54b7372300521564a
Jay Tejada <20103300@usc.edu.ph>
2023-01-05 22:58:15 +0800

    added assignment isNotif

--- backend/teacher/create_assignment.php
+++ backend/teacher/create_assignment.php
@@ -10,7 +10,7 @@
         $max     = $_POST['total_score'];
         $type    = $_POST['type'];
         $inst    = $_POST['instructions'];
-        $isNotif = true; //$_POST['isNotif'];
+        $isNotif = isset($_POST['isNotif']) ? TRUE : FALSE;
 
         $sql = $db->prepare("INSERT INTO assignments (sg_id, assignment_title, assignment_instruction, max_score, submission_type, close_datetime) VALUES (?,?,?,?,?,?)");
         $sql->bind_param("ississ", $sg_id, $title, $inst, $max, $type, $due);
@@ -28,12 +28,14 @@
                 $subject = "Assignment Created";
 
                 include("../notification/main_notif.php");
-            }
 
-            if($notif->execute())
+                if($notif->execute())
+                    header("location: ../../frontend/courses/assignments.php?m=sucess");
+                else
+                    header("location: ../../frontend/courses/assignments.php?m=notifFailed");
+            }else{
                 header("location: ../../frontend/courses/assignments.php?m=sucess");
-            else
-                header("location: ../../frontend/courses/assignments.php?m=notifFailed");
+            }
 
         } else {
             echo "\nError saving assignment: " . $db->error;
