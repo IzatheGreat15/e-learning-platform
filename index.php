c9e75555197b43ee7639eaebd0367f7af52f672d
Jay Tejada <20103300@usc.edu.ph>
2023-01-16 13:28:38 +0800

    push

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     $password = "";
 
     $conn = new mysqli($hostname, $username, $password);
-    if($conn->query("SELECT schema_name from information_schema.schemata WHERE schema_name = 'elearn_db'")->num_rows < 1)
-        include_once("backend/db/RunAtFirstUse.php");
+    include_once("backend/db/RunAtFirstUse.php");
         
     header("location: frontend/index.php");
 ?>
