62b3f0689960b380ee3e55fde344d56d5dfeec33
Jay Tejada <20103300@usc.edu.ph>
2023-01-16 08:27:14 +0800

    tyty

--- backend/student/send_assignment_response.php
+++ backend/student/send_assignment_response.php
@@ -17,12 +17,16 @@
           $response_answer = uploadFileSingle($_FILES["file"], "assignment/".$_SESSION['sg_id']."_".$assignment_id);
           $ar_id = mysqli_fetch_assoc($db->query("SELECT id from assignment_responses ORDER BY id DESC"))['id'];
           $sql = $db->prepare("INSERT INTO assignment_files (ar_id, file_location) VALUES (?,?)");
-          $sql->bind_param("is", $ar_id, $response_answer);
+          $sql->bind_param("is", $ar_id, $_SESSION['sg_id']."_".$assignment_id."/".$response_answer);
           if($sql->execute()){
             echo "Reply saved successfully"; 
           }
         }else{
-          
+          $user = mysqli_fetch_assoc($db->query('SELECT * FROM users WHERE id = '.$_SESSION['user_id']));
+          $filename = $user['id']."_".$user['fname']."_".$user['lname']."_".date("F d, Y", time()).".txt";
+          $file = fopen("../../frontend/files/assignment/".$_SESSION['sg_id']."_".$assignment_id."/".$filename, "w");
+          fwrite($file, $_POST['reply']);
+          fclose($file);
         }
         header("location: ../../frontend/courses/assignment.php?id=".$assignment_id);
       }else{
