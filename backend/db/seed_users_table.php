8b404c6aef83d7759831d6c5fd6532a0f3bc1639
Jay Tejada <20103300@usc.edu.ph>
2023-01-16 22:00:21 +0800

    push

--- backend/db/seed_users_table.php
+++ backend/db/seed_users_table.php
@@ -3,9 +3,9 @@
 // sql to create table
 $sql = "INSERT users (fname,lname,role,address,contact_num,email,password, token) VALUES
                     ('sample', 'admin', 'ADMIN', 'home', '09954119907', 'sample_admin@gmail.com', '".password_hash('password', PASSWORD_DEFAULT)."', '".password_hash('passwordsample_admin@gmail.com', PASSWORD_DEFAULT)."'),
-                    ('sample', 'admin', 'STUDENT', 'home', '09954119907', 'sample_student@gmail.com', '".password_hash('password', PASSWORD_DEFAULT)."', '".password_hash('passwordsample_student@gmail.com', PASSWORD_DEFAULT)."'),
-                    ('sample', 'admin', 'TEACHER', 'home', '09954119907', 'sample_teacher@gmail.com', '".password_hash('password', PASSWORD_DEFAULT)."', '".password_hash('passwordsample_teacher@gmail.com', PASSWORD_DEFAULT)."'),
-                    ('sample', 'admin', 'REGISTRAR', 'home', '09954119907', 'sample_registrar@gmail.com', '".password_hash('password', PASSWORD_DEFAULT)."', '".password_hash('passwordsample_registrar@gmail.com', PASSWORD_DEFAULT)."')";
+                    ('sample', 'student', 'STUDENT', 'home', '09954119907', 'sample_student@gmail.com', '".password_hash('password', PASSWORD_DEFAULT)."', '".password_hash('passwordsample_student@gmail.com', PASSWORD_DEFAULT)."'),
+                    ('sample', 'teacher', 'TEACHER', 'home', '09954119907', 'sample_teacher@gmail.com', '".password_hash('password', PASSWORD_DEFAULT)."', '".password_hash('passwordsample_teacher@gmail.com', PASSWORD_DEFAULT)."'),
+                    ('sample', 'registrar', 'REGISTRAR', 'home', '09954119907', 'sample_registrar@gmail.com', '".password_hash('password', PASSWORD_DEFAULT)."', '".password_hash('passwordsample_registrar@gmail.com', PASSWORD_DEFAULT)."')";
                     // ('sample', 'student01', 'STUDENT', 'home', '09776919206', 'sample_student01@gmail.com', '".password_hash('password', PASSWORD_DEFAULT)."'),
                     // ('sample', 'student02', 'STUDENT', 'home', '09776919206', 'sample_student02@gmail.com', '".password_hash('password', PASSWORD_DEFAULT)."'),
                     // ('sample', 'student03', 'STUDENT', 'home', '09776919206', 'sample_student03@gmail.com', '".password_hash('password', PASSWORD_DEFAULT)."'),
