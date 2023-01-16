66d2d2b48e5cffefd42c5dc870b2fd7353935a27
Jay Tejada <20103300@usc.edu.ph>
2023-01-16 18:23:32 +0800

    email verification

--- backend/verify_account.php
+++ backend/verify_account.php
@@ -1,20 +1,21 @@
-<?
+<?php
 
 include("config.php");
 session_start();
 
-if(isset($_GET['$token'])){
+if(isset($_GET['token'])){
     $token = $_GET['token'];
-    if($db->query("UPDATE users SET verified_on = CURRENT_TIMESTAMP where token = ".$token)){
-        $user = mysqli_fetch_assoc($db->query("SELECT * FROM users WHERE token = ".$token));
+    echo var_dump($token);
+    if($db->query("UPDATE users SET verified_on = CURRENT_TIMESTAMP where token = '".$token."'")){
+        $user = mysqli_fetch_assoc($db->query("SELECT * FROM users WHERE token = '".$token."'"));
 
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['role'] = $user['role'];
-        $_SESSION['isVerified'] = $user['verified_on'] == NULL ? "FALSE" : "TRUE";
+        $_SESSION['isVerified'] = "TRUE";
 
         header("location: ../frontend/verify-email.php?msg=accountVerified");
     }else{
-        header("location: ../frontend/vindex.php?msg=verificationFailed&spec=tokenNotFound");
+        header("location: ../frontend/index.php?msg=verificationFailed&spec=tokenNotFound");
     }
 }else{
     header("location: ../frontend/index.php?msg=verificationFailed&spec=noToken");
--- frontend/css/unverified.php
+++ frontend/css/unverified.php
@@ -1,13 +1,13 @@
 <!-- MODAL FOR UNVERIFIED ACCOUNTS -->
 <!-- ONLY IF THE ACCOUNT IS NOT YET VERIFIED -->
 <?php if($_SESSION["isVerified"] == "FALSE"): ?>
-<div id="modal-delete" class="modal-bg" style="display: block;">
+<div id="modal-delete" class="modal-bg" style="display: block; z-index: 300">
     <div class="modal-body">
         <span class="close">&times;</span>
         <div class="centered-align flex-col">
             <h3>Your account has not yet been verified.</h3>
-            <form action="../../backend/send_email_verification.php" method="POST">
-                <input type="hidden" name="email" value="<?= $user['email'] ?>">
+            <form action="<?= $url ?>backend/send_verification_email.php" method="POST">
+                <input type="hidden" name="email" value="<?= mysqli_fetch_assoc($db->query("SELECT email FROM users WHERE id = ".$_SESSION['user_id']))['email'] ?>">
                 <button type="submit" name="submit" class="blue">VERIFY ACCOUNT</button>
             </form>
         </div>
