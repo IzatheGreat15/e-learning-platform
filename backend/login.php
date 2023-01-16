61ea37de8b73e9cd0a0a2e2e79b7a212623a29b5
Jay Tejada <20103300@usc.edu.ph>
2023-01-16 11:25:04 +0800

    star

--- backend/login.php
+++ backend/login.php
@@ -7,7 +7,7 @@
       $password = $_POST["password"];
 
 
-      $sql = "SELECT id, role, password FROM users WHERE email = '".$email."'";
+      $sql = "SELECT * FROM users WHERE email = '".$email."'";
       $s = $db->query($sql);
       if($s->num_rows > 0){
          foreach($s as $user){
