429aa3fb5d84ab4482ef01e18567d5fd7c5d7eda
Jay Tejada <20103300@usc.edu.ph>
2023-01-16 22:09:05 +0800

    push

--- frontend/css/unverified.php
+++ frontend/css/unverified.php
@@ -5,7 +5,7 @@
     <div class="modal-body">
         <div class="centered-align flex-col">
             <h3>Your account has not yet been verified.</h3>
-            <form action="<?= $url ?>backend/send_verification_email.php" method="POST">
+            <form action="<?= $url ?>/backend/send_verification_email.php" method="POST">
                 <input type="hidden" name="email" value="<?= mysqli_fetch_assoc($db->query("SELECT email FROM users WHERE id = ".$_SESSION['user_id']))['email'] ?>">
                 <button type="submit" name="submit" class="blue">VERIFY ACCOUNT</button>
             </form>
